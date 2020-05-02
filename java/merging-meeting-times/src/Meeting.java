da2c0a771d7c692632bb75607d822e14c1ee4297
mmb <martin.m.borizanov@gmail.com>
2020-05-02 09:33:40 +0100

    Cleanup after code inspection

--- java/merging-meeting-times/src/Meeting.java
+++ java/merging-meeting-times/src/Meeting.java
@@ -1,60 +1,34 @@
-import javax.imageio.ImageReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class Meeting {
 
     private int startTime;
     private int endTime;
 
-    public Meeting(int startTime, int endTime){
+    public Meeting(int startTime, int endTime) {
         // number of 30 min blocks past 9:00am
         this.startTime = startTime;
         this.endTime = endTime;
     }
 
-    public int getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime( int startTime ) {
-        this.startTime = startTime;
-    }
-
-    public int getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime( int endTime ) {
-        this.endTime = endTime;
-    }
-
-
-    public boolean mergeableWith( Meeting anotherMeeting ){
-
-        //find out the order of the two meetings
-        Meeting earlierMeet = this.getStartTime() < anotherMeeting.getStartTime() ? this : anotherMeeting;
-        Meeting laterMeet = earlierMeet == this ? anotherMeeting : this;
-
-        return earlierMeet.getEndTime() >= laterMeet.getStartTime();
-
-    };
-
-
-    public static Meeting[] merge( Meeting meetings[] ) throws Exception {
+    public static Meeting[] merge(Meeting[] meetings) {
 
         List<Meeting> sortedMeetings = new ArrayList<>();
         Collections.addAll(sortedMeetings, meetings);
-        Collections.sort( sortedMeetings, Comparator.comparingInt(Meeting::getStartTime));
+        sortedMeetings.sort(Comparator.comparingInt(Meeting::getStartTime));
 
-        List<Meeting> mergedMeetings = new ArrayList<Meeting>();
+        List<Meeting> mergedMeetings = new ArrayList<>();
         mergedMeetings.add(sortedMeetings.get(0));
 
-        for( Meeting currentMeeting : sortedMeetings ){
+        for (Meeting currentMeeting : sortedMeetings) {
 
-            Meeting previousMergedMeeting = mergedMeetings.get( mergedMeetings.size()-1 );
+            Meeting previousMergedMeeting = mergedMeetings.get(mergedMeetings.size() - 1);
 
-            if( previousMergedMeeting.mergeableWith(currentMeeting) ){
-                previousMergedMeeting.setEndTime( currentMeeting.getEndTime() );
+            if (previousMergedMeeting.mergeableWith(currentMeeting)) {
+                previousMergedMeeting.setEndTime(currentMeeting.getEndTime());
             } else {
                 mergedMeetings.add(currentMeeting);
             }
@@ -62,6 +36,32 @@ public class Meeting {
         }
 
         return mergedMeetings.toArray(new Meeting[0]);
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(int startTime) {
+        this.startTime = startTime;
+    }
+
+    public int getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(int endTime) {
+        this.endTime = endTime;
+    }
+
+    public boolean mergeableWith(Meeting anotherMeeting) {
+
+        //find out the order of the two meetings
+        Meeting earlierMeet = this.getStartTime() < anotherMeeting.getStartTime() ? this : anotherMeeting;
+        Meeting laterMeet = earlierMeet == this ? anotherMeeting : this;
+
+        return earlierMeet.getEndTime() >= laterMeet.getStartTime();
+
     }
 
     @Override

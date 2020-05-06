2b74518d2e5ca8a2131bd04b63c09ccd2373a3c9
mmb <martin.m.borizanov@gmail.com>
2020-05-06 08:48:27 +0100

    Binary Expression Tree able to parse precedence, without brackets

---
+++ java/binary-expression-tree/src/BinaryExpressionTree.java
@@ -0,0 +1,65 @@
+public class BinaryExpressionTree {
+
+    private class Node {
+
+        public char contents;
+        public Node left;
+        public Node right;
+
+        public boolean isLeaf(){
+            return (left==null && right==null);
+        }
+    }
+
+    public Node root;
+
+    public Node buildExpressionTree(String expression ){
+
+        Node focusNode = new Node();
+        if(root == null ) root = focusNode;
+
+        if( expression.length() == 1 ){
+            focusNode.contents = expression.charAt(0);
+            return focusNode;
+        };
+
+        char nextOperator = '\0';
+        int  nextOperatorPosition = -1;
+
+        // Model precedence, in reverse order
+        String charsToCheck = expression.contains("+") || expression.contains("-") ? "+-" : "/*";
+
+        // Finds the rightmost operator
+        for( int idx = 0; idx < expression.length(); idx ++ ) {
+
+            char thisChar = expression.charAt(idx);
+
+            if (charsToCheck.indexOf(thisChar) != -1) {
+                nextOperator = thisChar;
+                nextOperatorPosition = idx;
+            }
+        }
+
+        focusNode.contents = nextOperator;
+
+        String leftSide = expression.substring( 0, nextOperatorPosition );
+        String rightSide = expression.substring( nextOperatorPosition + 1 );
+
+        focusNode.left  = buildExpressionTree( leftSide );
+        focusNode.right = buildExpressionTree( rightSide );
+
+        return focusNode;
+    }
+
+    public static void main(String[] args) {
+
+        String expression = "1*2/3+4/5*6+7-8*9";
+
+        BinaryExpressionTree binaryExpressionTree = new BinaryExpressionTree();
+        binaryExpressionTree.buildExpressionTree(expression);
+
+        Node root = binaryExpressionTree.root;
+
+        System.out.println("Blah!");
+    }
+}
